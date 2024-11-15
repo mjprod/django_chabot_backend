0c6101890042d7bbe6929d3c437bcf4b70e48de7
JSaundersai <Jareds.ai@icloud.com>
2024-11-15 12:43:03 +1000

    added translate_en_to_ms functions for taking in the generation output, changed the API's User input to include user_id(default=0) and to send back a json payload with a translations array with malay as the key and the value being the output, updated ViewSummary to also fit the new struct

--- backend/chatbot_dir/api/serializers.py
+++ backend/chatbot_dir/api/serializers.py
@@ -3,6 +3,8 @@ from rest_framework import serializers
 
 class UserInputSerializer(serializers.Serializer):
     prompt = serializers.CharField()
+    # added user_id field for when users are added
+    user_id = serializers.IntegerField(default=0)
 
 
 class AIResponseSerializer(serializers.Serializer):
@@ -23,17 +25,20 @@ class IncorrectAnswerResponseSerializer(serializers.Serializer):
 
 class CaptureSummarySerializer(serializers.Serializer):
     user_input = serializers.CharField()
-    ai_response = serializers.CharField()
+    ai_response = serializers.DictField()
     correct_bool = serializers.BooleanField()
     chat_rating = serializers.IntegerField(min_value=0, max_value=6)
     incorrect_answer_response = serializers.CharField(required=False, allow_blank=True)
     metadata = serializers.DictField(required=False)
 
 
+# added the english and malay fields to the view summary call
 class ViewSummarySerializer(serializers.Serializer):
     Date_time = serializers.DateTimeField()
+    User_id = serializers.IntegerField(default=0)
     Question = serializers.CharField()
-    Answer = serializers.CharField()
+    Answer_English = serializers.CharField()
+    Answer_Malay = serializers.CharField()
     Question_correct = serializers.BooleanField()
     Correct_rating = serializers.IntegerField(min_value=1, max_value=5)
     Correct_Answer = serializers.CharField()
