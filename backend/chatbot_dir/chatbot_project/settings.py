94712fec86333e7b80f24d8a1ef0d3abf6a79bbd
JSaundersai <Jareds.ai@icloud.com>
2024-11-19 13:20:31 +1000

    added staging to settings for CORS

--- backend/chatbot_dir/chatbot_project/settings.py
+++ backend/chatbot_dir/chatbot_project/settings.py
@@ -95,6 +95,7 @@ WSGI_APPLICATION = "chatbot_project.wsgi.application"
 CORS_ALLOW_ALL_ORIGINS = False
 CORS_ALLOWED_ORIGINS = [
     "https://api-staging.mjproapps.com",
+    "https://staging.mjproapps.com",
 ]
 
 
