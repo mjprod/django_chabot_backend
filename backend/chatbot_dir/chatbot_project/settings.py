afe0c416e6cc97a96e8cdad51be99c80a828d219
JSaundersai <Jareds.ai@icloud.com>
2024-11-20 09:22:31 +1000

    changed settings file back

--- backend/chatbot_dir/chatbot_project/settings.py
+++ backend/chatbot_dir/chatbot_project/settings.py
@@ -92,7 +92,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "chatbot_project.wsgi.application"
 
-CORS_ALLOW_ALL_ORIGINS = False
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "https://api-staging.mjproapps.com",
 ]
