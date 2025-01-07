36567878a7986e0882ad8e034ed4591aa6fedf68
GlaucoInfinities <162525681+GlaucoInfinities@users.noreply.github.com>
2025-01-07 21:21:58 +1000

    ignore

--- settings/staging.py
+++ settings/staging.py
@@ -22,13 +22,6 @@ MONGODB_PASSWORD = "JS19931989JR$"
 MONGODB_CLUSTER = "staging-cluster.ooyoj.mongodb.net"
 MONGODB_DATABASE = "chatbotdb"
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = SECRET_KEY
-
-ROOT_URLCONF=ROOT_URLCONF
-INSTALLED_APPS=INSTALLED_APPS
-MIDDLEWARE=MIDDLEWARE
-
 # Construct MongoDB URI
 MONGODB_URI = (
     f"mongodb+srv://{MONGODB_USERNAME}:{MONGODB_PASSWORD}"
@@ -38,7 +31,7 @@ MONGODB_URI = (
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3", # noqa: F405
         "CONN_MAX_AGE": 60,
         "OPTIONS": {
             "timeout": 20,
