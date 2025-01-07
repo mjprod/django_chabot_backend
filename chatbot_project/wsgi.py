9488800bbd05a84f00f6998a8b1ef7abb4b27a5f
GlaucoInfinities <162525681+GlaucoInfinities@users.noreply.github.com>
2025-01-07 20:03:26 +1000

    files created

--- chatbot_project/wsgi.py
+++ chatbot_project/wsgi.py
@@ -13,13 +13,13 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 # Set environment variable before application loads
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chatbot_project.settings")
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chatbot_project.settings")
 
 
 # Initialize WSGI application with memory management
 def get_managed_application():
     application = get_wsgi_application()
-    gc.collect()  # Force garbage collection after initialization
+    # gc.collect()  # Force garbage collection after initialization
     return application
 
 
@@ -27,6 +27,7 @@ application = get_managed_application()
 
 
 # Add request cleanup middleware
+'''
 class MemoryCleanupMiddleware:
     def __init__(self, application):
         self.application = application
@@ -37,6 +38,6 @@ class MemoryCleanupMiddleware:
         finally:
             gc.collect()  # Clean up after each request
 
-
+'''
 # Wrap application with memory cleanup
-application = MemoryCleanupMiddleware(application)
+# application = MemoryCleanupMiddleware(application)
